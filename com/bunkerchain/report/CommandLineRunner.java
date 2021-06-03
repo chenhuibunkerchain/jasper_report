ecad3617628fe0a0a409105c60cee59fb9a3e1b3
Yong Sheng <yongsheng.lye@bunkerchain.io>
2021-06-03 15:16:16 +0800

    Update Jasper

--- com/bunkerchain/report/CommandLineRunner.java
+++ com/bunkerchain/report/CommandLineRunner.java
@@ -3,9 +3,12 @@ public class CommandLineRunner {
 
     public static void main(String[] args) throws Exception {
 
-        SafetyCheckListGenerate app2 = new SafetyCheckListGenerate();
-        app2.start();
+//        SafetyCheckListGenerate app2 = new SafetyCheckListGenerate();
+//        app2.start();
 //        SealChecklistGenerate app = new SealChecklistGenerate();
 //        app.start();
+        
+        TimeSheetGenerator app3 = new TimeSheetGenerator();
+        app3.start();
     }
 }
